d1d77783365ec1b079b89c1fd23951a86e8fbac2
Steven <stevnn.yee@gmail.com>
2025-07-26 19:31:10 -0400

    created the footer for the site

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import CardList from '../components/CardList'
+import Footer from '../components/Footer'
 
 const Homepage = () => {
   return (
@@ -9,6 +10,7 @@ const Homepage = () => {
         <CardList/>
         <CardList/>
         <CardList/>
+        <Footer/>
     </div>
   )
 }
