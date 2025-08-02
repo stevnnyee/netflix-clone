a6037fe438e6c3d0cb63f4071206c96120a41295
Steven <stevnn.yee@gmail.com>
2025-08-02 14:36:06 -0400

    final backend changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,18 @@ import Navbar from './components/Navbar'
 import Homepage from './pages/Homepage'
 import { Route, Routes } from "react-router"
 import Moviepage from './pages/Moviepage'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path={"/"} element={<Homepage/>}/>
-        <Route path={"/movie/:id"} element={<Moviepage/>}/>
+        <Route path={"/"} element={<Homepage />} />
+        <Route path={"/movie/:id"} element={<Moviepage />} />
+        <Route path={"/signin"} element={<SignIn />} />
+        <Route path={"/signup"} element={<SignUp />} />
       </Routes>
     </div>
   )
